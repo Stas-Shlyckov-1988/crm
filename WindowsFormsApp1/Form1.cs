c6093d39704f7d6747f3f7708ae2e961e6a1a8fd
Stas-Shlyckov-1988 <stas.shlyckov@yandex.ru>
2024-10-04 10:59:54 +0300

    add projects

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var i = listBox1.Items[listBox1.SelectedIndex].ToString();
+            switch(i.ToString())
+            {
+                case "тест1":
+                    pictureBox1.Visible = true;
+                    pictureBox2.Visible = false;
+                    pictureBox3.Visible = false;
+                    break;
+                case "тест2":
+                    pictureBox1.Visible = false;
+                    pictureBox2.Visible = true;
+                    pictureBox3.Visible = false;
+                    break;
+                default:
+                    pictureBox1.Visible = false;
+                    pictureBox2.Visible = false;
+                    pictureBox3.Visible = true;
+                    break;
+            }
+        }
+
+    }
+}
